8d68b686abffd598c867f5ee800daf00a5910751
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-28 09:24:45 +0000

    basic training by Aoj

--- Ruby/topic5/question1.rb
+++ Ruby/topic5/question1.rb
@@ -1,7 +1,7 @@
-r="#"
-loop{
-	h,w=gets.split(" ").map(&:to_i)
-	break if h==0 and w==0
-	h.times{puts r*w}
-	puts ""
+r = "#"
+loop {
+  h,w = gets.split.map(&:to_i)
+  break if h == 0 && w == 0
+  h.times{puts r*w}
+  puts ""
 }
