52fd03bf848a9d4cda2fc7a191ebcd1ebeb9ca40
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-06 08:59:13 +0000

    modify answer

--- Ruby/topic7/question2.rb
+++ Ruby/topic7/question2.rb
@@ -1,5 +1,9 @@
-loop {
-  n,x = gets.split.map(&:to_i)
-  break if x == 0
-  puts (1..n).to_a.combination(3).count{ |a,b,c| a+b+c==x }
-}
+while true
+  n,x = gets.chomp.split.map(&:to_i)
+  break if n==0 && x==0
+  
+  numbers = (1..n).to_a
+  count = 0
+  numbers.combination(3){ |a,b,c| count += 1 if a+b+c==x }
+  puts count
+end
