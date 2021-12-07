cafac60496aa073005fec61f1ad038000be97b07
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-07 02:32:25 +0000

    modified

--- ITP1/topic7/question3.rb
+++ ITP1/topic7/question3.rb
@@ -1,6 +1,20 @@
-r,c = gets.split.map(&:to_i)
-r.times do
-  c.times do
-    gets.to
-  end
+n,x = gets.chomp.split.map(&:to_i)
+a =[]
+n.times do
+    a_i,b_i,c_i,d_i=gets.split.map(&:to_i)
+    if x<=a_i
+        a << b_i
+    else
+      count = (x - a_i).to_f/c_i
+      if count.to_f <= 1
+        price = b_i + d_i
+        a << price.to_i
+      else
+        price = count.to_i * d_i + b_i
+        a << price.to_i
+      end
+    end
 end
+min = a.min
+max = a.max
+puts"#{min} #{max}"
