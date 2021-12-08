9f1298cdb63b0bc0ffc0d8389db8aae8cff43d7c
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-08 09:45:42 +0000

    resolve this question

--- paiza/c_098.rb
+++ paiza/c_098.rb
@@ -1,9 +1,20 @@
 n = gets.to_i
-ball = [*(1..n).map{ gets.split.map(&:to_i) }]
+ball = Array.new(n)
+n.times do |i|
+  ball[i] = gets.to_i
+end
 m = gets.to_i
 m.times do
-  x,y,c = gets.split.map(&:to_i)
-  ball << ball[x-1] - c
-  ball << ball[y-1] + c
+  x,y,c = gets.chomp.split.map(&:to_i)
+  if ball[x-1] < c
+    ball[y-1] += ball[x-1]
+    ball[x-1] = 0
+  
+  else  
+    ball[x-1] -= c
+    ball[y-1] += c
+  end
 end
-puts ball
+n.times do |i|
+  puts ball[i]
+end
