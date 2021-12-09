028d644034d872e5e1cac1cdd4c1268359382c5d
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-09 13:28:45 +0000

    modfiy

--- paiza/b_096.rb
+++ paiza/b_096.rb
@@ -1,16 +1,8 @@
 h,w = gets.chomp.split.map(&:to_i)
 s = 0
 numbers = []
+array = []
 h.times do |i|
-  array = []
-  w.times do |j|
-    array[j] = gets.to_s
-  end
-  if array.include?("#")
-    s += w
-  end
-  numbers << array.index("#").to_i
+  array << gets.chomp.split.map(&:to_s)
 end
-numbers.uniq!
-s_2 = numbers.size.to_i * 3
-puts s + a_2
+puts array
