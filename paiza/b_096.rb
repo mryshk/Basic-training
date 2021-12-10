1c71fa46a1b76e54e6aa162a6cde309ddd73797d
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-10 02:01:42 +0000

    resolve this question

--- paiza/b_096.rb
+++ paiza/b_096.rb
@@ -2,22 +2,30 @@ h,w = gets.chomp.split.map(&:to_i)
 s = 0
 numbers = []
 array = []
-c = 0
 h.times do |i|
+  # h行分の入力を配列にして取得
   array[i] =  gets.chomp.chars
+  # １行ずつ#を含んでいるか確認する
   if array[i].include?("#")
+    # 含んでいればそのindexをnumbers配列へ入れ込む。
     array[i].each_with_index do |item, i|
       if item == "#"
         numbers << i
       end
     end
+  end
+end
+# 爆弾の影響を受ける部分を全て#に変えていく。
+h.times do |i|
+  if array[i].include?("#")
+    # 行全てを変更
     array[i].fill("#")
-  
   else
     numbers.each do |j|
-      array[i][j] = "#"
+      # 列全てを順に変えていく。
+      array[i][j] ="#"
     end
   end
 end
+# 最後に#の数を数えて終わる。
 puts array.join("").count("#")
-
