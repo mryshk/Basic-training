59f5c233296d28f9808b31ff0cd53012245329a8
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-11 08:00:28 +0000

    no solution

---
+++ paiza/b_092.rb
@@ -0,0 +1,38 @@
+h,w,k = gets.chomp.split.map(&:to_i)
+a = []
+h.times do |i|
+  a[i] = gets.chomp.chars
+end
+save = []
+array = []
+for j in 0..(h-1) do
+  a[j].each_with_index do |item, i|
+    if item=="N"
+      save.push(j,i)
+    elsif item != "#"
+      array << [j,i]
+    end
+  end
+end
+result=[]
+subject =[]
+array.each_with_index do |item,i|
+  for j in 0..1
+    result << (save[j] - array[i][j]).abs
+  end
+end
+re = []
+# result.each_with_index do |item , i|
+#   re << result[i].sum
+# end
+result = result.each_slice(2).to_a
+re = result.map(&:sum)
+subject = []
+re.each_with_index do |item,i|
+  if item == re.min
+    subject << array[i]
+  end
+end
+subject.each do |i|
+ puts a[]
+end
