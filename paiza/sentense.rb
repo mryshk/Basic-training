d29bbd7abe62be9015a8192de1df9e692ff3c9b2
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-16 13:21:42 +0000

    modify comment

--- paiza/sentense.rb
+++ paiza/sentense.rb
@@ -16,35 +16,41 @@
       two_words_connector: " and ",
       last_word_connector: ", and "
     }
-    # もし引数optionsにkeyのlocaleが存在する場合 かつ I18nに定義されている場合
+    
+    
+    # もし引数optionsにkeyのlocaleが存在する場合 かつ I18nが定義されている場合
     # I18nとは簡単にいうと、言語の変換機能
     if options[:locale] != false && defined?(I18n)
-      # support以下のarray部分の言語を引数optionsに存在する言語（:locale）に翻訳する。 翻訳したものをハッシュ型でi18n_connectors変数へ格納。
+      # config/locales/ymlファイルにsupport.array配下に[:locale]で指定した言語で接続言語を定義する。
+      # そのymlファイルで定義した言語をi18n_connectors変数へ格納する。
       i18n_connectors = I18n.translate(:'support.array', locale: options[:locale], default: {})
-      # デフォルトの接続言葉(default)に上記の翻訳(i18n)バージョンをくっつける(mergeする)。！がついているため値が変更される。（破壊的メソッドであるため。）
+      # デフォルトの接続言葉(default)に上記の翻訳(i18n)バージョンをくっつける(mergeする)。
+      # ！がついているため値が変更される。（破壊的メソッドであるため。）
       default_connectors.merge!(i18n_connectors)
     end
-    # options変数へ引数のハッシュをマージ（破壊的）する。
-    # keyが重複する場合、後に追加したkeyが優先される。すなわち、引数optionsにdefaultと重複するkeyが存在する場合、
-    # 引数optionsのvalueが優先される。
+    
+    
+    # デフォルトの接続言葉(default)に引数のハッシュ(options)をマージ（破壊的）してoptions変数を更新する。
+    # merge・・keyが重複する場合、後に追加したkeyが優先される。
+    # すなわち、引数optionsにdefaultと重複するkeyが存在する場合、引数optionsのkeyのvalueが優先される。
     # default {two_words_connctor: "and"} が 引数options {two_words_connector: "と"}に変更される（破壊的に）
     options = default_connectors.merge!(options)
 
    # レシーバ（配列）の要素数の場合分け。
     case length
-    # 要素数が０の場合,空白""を表示
+    # 配列要素数が０の場合,空白""を表示
     when 0
       +""
-    # 要素数が１の場合
+    # 配列要素数が１の場合
     # レシーバの配列が０の要素をそのまま表示。
     when 1
       +"#{self[0]}"
-    # 要素数が２の場合
+    # 配列要素数が２の場合
     # レシーバの配列が０の要素を表示し、次にハッシュoptionsのkey[:two_words_connector]のvalueを変数展開し表示。
     # その後、レシーバの配列が１の要素を表示。
     when 2
       +"#{self[0]}#{options[:two_words_connector]}#{self[1]}"
-    # 要素数が３以上の場合
+    # 配列要素数が３以上の場合
     else
     # レシーバの配列が０の要素〜最後から２つ目の要素までを、ハッシュoptionsのkey[:words_connector]のvalueとくっつけて表示。
     # その後に、ハッシュoptionsのkey[:last_words_connector]のvalueを変数展開し表示。残りの配列の最後の要素を表示。
